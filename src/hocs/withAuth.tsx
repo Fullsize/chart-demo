9014320424a8030be7f8416b9d1edc24a6b86276
无涯 <1619686166@qq.com>
2024-11-12 16:05:22 +0800

    feat: 跳过登录检测

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -5,29 +5,29 @@ import { getQueryString, removeQueryParams } from '@/utils';
 import { getUrlSearchParameter } from '@/utils/url';
 const WithAuth = (Component: any) => {
     return function Auth() {
-        const navigate = useNavigate();
-        const location = useLocation();
-        useEffect(() => {
-            // SSO 登录逻辑
-            if (window.isSsoLogin) {
-                const ticketPram = getUrlSearchParameter('smart-jwt');
-                if (ticketPram) {
-                    navigate('/login');
-                    return;
-                }
-            }
+        // const navigate = useNavigate();
+        // const location = useLocation();
+        // useEffect(() => {
+        //     // SSO 登录逻辑
+        //     if (window.isSsoLogin) {
+        //         const ticketPram = getUrlSearchParameter('smart-jwt');
+        //         if (ticketPram) {
+        //             navigate('/login');
+        //             return;
+        //         }
+        //     }
 
-            const token = getToken();
-            const { smart_token }: any = getQueryString();
-            if (!token && !smart_token) {
-                navigate('/login');
-                return;
-            }
-            if (smart_token) {
-                setToken(smart_token);
-                removeQueryParams('smart_token');
-            }
-        }, [location.pathname]);
+        //     const token = getToken();
+        //     const { smart_token }: any = getQueryString();
+        //     if (!token && !smart_token) {
+        //         navigate('/login');
+        //         return;
+        //     }
+        //     if (smart_token) {
+        //         setToken(smart_token);
+        //         removeQueryParams('smart_token');
+        //     }
+        // }, [location.pathname]);
         return <Component />;
     };
 };
